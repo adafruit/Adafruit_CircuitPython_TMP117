a9c067500bc946f62d43153ad4cbc81583f7c2a3
jposada202020 <jquintana202020@gmail.com>
2023-04-12 14:21:21 -0400

    Single Measurement Test Update

--- examples/tmp117_single_measurement_test.py
+++ examples/tmp117_single_measurement_test.py
@@ -8,6 +8,9 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 tmp117 = adafruit_tmp117.TMP117(i2c)
 
+# Values here for the average_measurements
+Average_Measure = {1: "AVERAGE_1X", 2: "AVERAGE_8X", 3: "AVERAGE_32X", 4: "AVERAGE_64X"}
+
 # uncomment different options below to see how it affects the reported temperature
 # and measurement time
 
@@ -18,7 +21,7 @@ tmp117 = adafruit_tmp117.TMP117(i2c)
 
 print(
     "Number of averaged samples per measurement:",
-    tmp117.averaged_measurements,
+    Average_Measure[tmp117.averaged_measurements],
 )
 
 while True:
